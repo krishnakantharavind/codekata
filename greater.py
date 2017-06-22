6d49ee494e882908c30d01d28ffd063153a9d113
krishnakantharavind <krishnakantharavind@gmail.com>
2017-06-22 09:59:30 +0530

    Update greater.py

--- greater.py
+++ greater.py
@@ -1,8 +1,11 @@
 a=input()
 b=input()
 c=input()
-if a>b and c:
-	print "a is greater"
+if a>b:
+	if a>c:
+		print "a is greater"
+	else:
+		print "c is greater"
 elif  b>c:
 	print "b is greater"
 else:
